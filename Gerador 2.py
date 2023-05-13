a646a03daade5eb55fc33b003f3e85f8939b13e7
Ana C <70434233+unicorncakebang@users.noreply.github.com>
2023-05-13 12:22:27 -0300

    Create Gerador 2.py

---
+++ Gerador 2.py
@@ -0,0 +1,6 @@
+#neste  cógido o usuário infomará a quantidade de números de 1 a 60 a serem colocados na lista.
+import random #pensador
+x = int(input("Quantos números ?")
+r = random.sample(range(0,60),x)
+
+print(f"Lista:{r}")
