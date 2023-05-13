c8168cfe8b4f8e23c5019016de73624b03b8e8a2
Ana C <70434233+unicorncakebang@users.noreply.github.com>
2023-05-13 12:19:35 -0300

    Create Geradorsimples.py

---
+++ Geradorsimples.py
@@ -0,0 +1,6 @@
+import random
+
+#ele irá escolher aleatóriamente números de 1 a 60 e criará uma lista com 6 números selecionados pelo sistema.
+r= random.sample(range(1,61),6)
+
+print(f" Lista : {r} ")
